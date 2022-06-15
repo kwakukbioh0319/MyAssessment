7c832e169c4ebb7e58e3265aab48ddcbcf57576d
kwaku kwarteng-bioh <kwaku.k.bioh@hotmail.com>
2022-06-14 21:57:02 -0400

    finish

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // Obtain the img element, and assign it to the image variable
-let image = "pet";
-image.src= "file:///Users/apple/Documents/rambo.%20Search_files/876bb7a8-e8dd-4e36-ab3a-f0b9aba942e5.jpg"
-    document.getElementById.appendChild(img)
+let image = document.getElementById('pet')
+// image.src= "file:///Users/apple/Documents/rambo.%20Search_files/876bb7a8-e8dd-4e36-ab3a-f0b9aba942e5.jpg"
+//     document.getElementById.append(img)
 
 // Obtain the 'shrink-grow' button, and assign it to the changeSize variable
-let changeSize 
+let changeSize = document.getElementById('shrink-grow')
 
 //DO NOT CHANGE THE FOLLOWING CODE
 changeSize.addEventListener("click", function() {
